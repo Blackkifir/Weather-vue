80fc2884920003251fc13339710d7464b2d6e573
Blackkifir <goldlamborgini@gmail.com>
2024-04-02 20:01:49 +0300

    Made an asynchronous request to the server using axios and created a loader.

--- src/components/DayOfWeek.vue
+++ src/components/DayOfWeek.vue
@@ -1,23 +1,41 @@
 <template>
-  <div class="dayOfWeek">
-    <div class="dayOfWeek_details">
-      <p
-        class="dayOfWeek_day"
-      >
-        31
-      </p>
-      <p
-        class="dayOfWeek_currentDay"
-      >
-        Sunday
-      </p>
+  <div>
+    <div class="dayOfWeek">
+      <div class="dayOfWeek_date">
+        <p class="dayOfWeek_day">31</p>
+        <p class="dayOfWeek_currentDay">Sunday</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+
   export default {
     name: 'day-of-week',
+
+    data() {
+      return {
+        items: [],
+      }
+    },
+    methods: {
+      async getDayOfWeek() {
+        try {
+          const api = 'https://api.openweathermap.org/data/2.5/forecast?q=London&appid=6552610f79c2b672097111aa772032b1';
+          const response = await axios.get(api);
+
+          this.items = response.data;
+          console.log(this.items);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+    mounted() {
+      this.getDayOfWeek();
+    }
   }
 </script>
 
@@ -29,21 +47,23 @@
     padding: 15px;
   }
 
-  .dayOfWeek_details {
+  .dayOfWeek_date {
     display: flex;
     margin-bottom: 10px;
   }
   
   .dayOfWeek_day,
   .dayOfWeek_currentDay {
-    border: 2px solid #FFFFFF;
+    border: none;
+    padding: 5px 10px;
+    background-color: #FFFFFF;
     font-size: 17px;
     line-height: 20px;
-    padding: 5px 10px;
+    color: #000000;
   }
 
   .dayOfWeek_day {
-    border-right: none;
+    border-right: 2px solid #000000;
   }
 
 </style>
